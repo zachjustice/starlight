45847e4cea1fd9a9bc49d87c5b5e1e2d84127e19
zach123 <zachjustice123@gmail.com>
2015-05-13 13:57:04 -0400

    Functional Navbar-- still needs styling

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
@@ -19,32 +18,29 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-	<nav>
-	    <ul>
-		<li><a href='/index.php'><img src='logo.png'></a></li>
-		<li>
-                    <a href="">Categories <space class="caret" /></a>
-		    <div>
-			<ul>
-			    <li><a href="">Tier 1</a></li>
-			    <li><a href="">Tier 1</a></li>
-			    <li><a href="">Tier 1</a></li>
+        <nav>
+            <ul>
+                <li><a href=""><img scr="/img/logo.png" /></a></li>
+                <li>
+                    <a href="">Products <span class="caret"></span></a>
+                    <div>
+                        <ul>
+                            <li><a href="">Tier 1</a></li>
+                            <li><a href="">Tier 1</a></li>
+                            <li><a href="">Tier 1</a></li>
                         </ul>
-		    </div>
-		</li>
-		<li><input placeholder="Search Products..." /></li>
-	    </ul>
-
-
-	</nav>
-	
-
+                    </div>
+                </li>
+                <li><input placeholder="Search Products..." /></li>
+            </ul>
+        </nav>
+            
 	<? for( $i = 0; $i < 3; $i++ ): ?>
 		<p>Hello world! This is HTML5 Boilerplate.</p>
 	<? endfor; ?>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/{{JQUERY_VERSION}}/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
 
