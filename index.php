1d12c437783af390117b581bd5a4500be3d5149b
Cloud User <ec2-user@ip-172-31-21-25.us-west-2.compute.internal>
2015-05-18 15:08:05 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Cloud User <ec2-user@ip-172-31-21-25.us-west-2.compute.internal>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   css/main.css
    modified:   index.php
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	css/main - Copy.css
    	index - Copy.php
    	starlight/

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!doctype html>
 <html class="no-js" lang="">
     <head>
+        <!--<title>Starlight</title>-->
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title><? echo( 'StarLight' ) ?></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>StarLight</title>
+       <!-- for FF, Chrome, Opera -->
+        <link rel="icon" type="image/png" href="img/favicon.ico" sizes="16x16">
+        <link rel="icon" type="image/png" href="img/favicon.ico" sizes="32x32">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+<!-- for IE -->
+        <link rel="icon" type="image/x-icon" href="/img/favicon.ico" >
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
         <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="css/main.css">
@@ -20,18 +24,48 @@
 
         <nav>
             <ul>
-                <li><a href=""><img scr="/img/logo.png" /></a></li>
-                <li>
-                    <a href="">Products <span class="caret"></span></a>
-                    <div>
-                        <ul>
-                            <li><a href="">Tier 1</a></li>
-                            <li><a href="">Tier 1</a></li>
-                            <li><a href="">Tier 1</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li><input placeholder="Search Products..." /></li>
+                <li id="icon"><a href="#"><img src="img/Starlight-icon.png" style="width:64px;height:56px"></a></li>
+                <li id="menu">
+            <a href="products.html">Products <span class="caret"></span></a>
+            <div id="tierone">
+                <ul>
+                    <li>
+                        <a href="products.html#chair">Lights <span class="caret"></span></a>
+                        <div>
+                            <ul>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="products.html#table">Security<span class="caret"></span></a>
+                        <div>
+                            <ul>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                            </ul>
+                        </div>
+                        </li>
+                    <li>
+                        <a href="cooker.html">Other<span class="caret"></a>
+                        <div>
+                            <ul>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                            </ul>
+                        </div>
+                        </li>
+                </ul>
+            </div>
+        </li>
+                <li id="search"><input placeholder="Search"/></li>
+                <li id="s_button"><a href="#"><img src="img/SearchButton.png" style="width:36px;height:36px"></a></li>
+                <li id="signin" class="c"><a href="#">sign in&#47register</a></li>
+                <li id="cart" class="c"><a href "#"><img src="img/cart.png" alt="Cart" style="width:30px;height:30px"></a></li> 
             </ul>
         </nav>
             
