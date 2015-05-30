b2f8622777d3c092282ee0d59278228a59ae397e
Cloud User <ec2-user@ip-172-31-21-25.us-west-2.compute.internal>
2015-05-30 14:18:58 -0400

    deleted:    apple-touch-icon.png 	modified:   css/main.css 	deleted:    favicon.ico

---
+++ products.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <!--<title>Starlight</title>-->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>StarLight</title>
+       <!-- for FF, Chrome, Opera -->
+        <link rel="icon" type="image/png" href="img/favicon.ico" sizes="16x16">
+        <link rel="icon" type="image/png" href="img/favicon.ico" sizes="32x32">
+
+<!-- for IE -->
+        <link rel="icon" type="image/x-icon" href="/img/favicon.ico" >
+        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico"/>
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/product.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div id="sidebar">
+            <ul class="filter">Search in:</ul>
+            <ul class="filter">Filter by</ul>
+            <ul>Price
+                <li>$<input placeholder="min">to  $<input placeholder="max"></li>
+            </ul>
+            <ul>header
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+            </ul>
+            <ul>header
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+            </ul>
+            <ul>header
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+                <li>subtext</li>
+            </ul>
+        </div>
+    <header>Products</header>
+        <div id="Table">
+            <ul>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product1.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product2.jpg" style="height:100%; width:100%;"></a></li>
+                    </div>
+                    <div class="product">
+                <li><a href="#"><p class="info">Product Name<br>Price</p><img src="img/top_products/product3.png" style="height:100%; width:100%;"></a></li>
+                    </div>
+            </ul>
+        </div>
+	<? for( $i = 0; $i < 1; $i++ ): ?>
+		<p></p>
+	<? endfor; ?>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
